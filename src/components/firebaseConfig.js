d40289d5ad9f1788448c98daa31764ebd0246a9f
Jeremy Lincoln <bossco28@gmail.com>
2017-12-15 16:28:11 -0500

    Add all components for employee

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCmw7BEEvalQu30_N8oDQOxGTLprMmBP4g",
+  authDomain: "vuefs-prod-321c4.firebaseapp.com",
+  databaseURL: "https://vuefs-prod-321c4.firebaseio.com",
+  projectId: "vuefs-prod-321c4",
+  storageBucket: "vuefs-prod-321c4.appspot.com",
+  messagingSenderId: "604250729066"
+}
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Dashboard from '@/components/Dashboard'
+import NewEmployee from '@/components/NewEmployee'
+import ViewEmployee from '@/components/ViewEmployee'
+import EditEmployee from '@/components/EditEmployee'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
+})
